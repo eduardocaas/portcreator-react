d51a67504b4eb14210491fa516d9f9b1314c95a9
Eduardo <eduardofc.dev@gmail.com>
2025-07-03 00:32:31 -0300

    Add structure

--- app/src/components/auth/Signin.tsx
+++ app/src/components/auth/Signin.tsx
@@ -1,9 +1,21 @@
+import { Button, Container, FormControl, FormLabel } from 'react-bootstrap'
 import './Signin.css'
 
 function SignIn() {
   return (
-   <div className='container'>
-    
-   </div>
+   <Container>
+    <form>
+      <div className='mb-1'>
+        <FormLabel htmlFor='inputEmail'>E-mail</FormLabel>
+        <FormControl type='email' id='inputEmail' required></FormControl>
+      </div> 
+      <div className="mb-1">
+        <FormLabel htmlFor='inputPassword'>Senha</FormLabel>
+        <FormControl type='password' id='inputPassword' required></FormControl>
+      </div>
+      <Button type='submit'>Entrar</Button>
+      <p>Não tem uma conta? <a>Registre-se</a></p>
+    </form>
+   </Container>
   )
 }
