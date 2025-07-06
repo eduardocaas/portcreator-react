165bb0f5f69a5859b0b90cb0fdc4c033116ea116
eduardocaas <eduardofc.dev@gmail.com>
2025-07-05 23:27:23 -0300

    Implemented auth - signin, signup methods

--- app/src/services/AuthService.ts
+++ app/src/services/AuthService.ts
@@ -1,7 +1,20 @@
 import axios from "axios"
+import type { Credentials } from "../models/auth/Credentials";
+import type { SignupModel } from "../models/auth/SignupModel";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-export class AuthService {
-  
+const httpOptions = {
+  headers: {
+    'Content-Type' : 'application/json'
+  }
+};
+
+const login = async (credentials: Credentials) => {
+  const response = await axios.post(`${API_URL}/auth/sign`, credentials, httpOptions);
+  return response.data;
+} 
+
+const signup = async (signupModel: SignupModel) => {
+  return axios.post(`${API_URL}/auth/signup`, signupModel, httpOptions);
 }
