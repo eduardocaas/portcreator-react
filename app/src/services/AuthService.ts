7f35960ea88a609cecf9b30d5f383d199499cdcb
eduardocaas <eduardofc.dev@gmail.com>
2025-07-06 13:21:30 -0300

    Implemented auth methods

--- app/src/services/AuthService.ts
+++ app/src/services/AuthService.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 import type { Credentials } from "../models/auth/Credentials";
 import type { SignupModel } from "../models/auth/SignupModel";
+import { jwtDecode } from "jwt-decode";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -17,4 +18,29 @@ const login = async (credentials: Credentials) => {
 
 const signup = async (signupModel: SignupModel) => {
   return axios.post(`${API_URL}/auth/signup`, signupModel, httpOptions);
+}
+
+const decodeToken = (token: string) => {
+  if (!token) return null;
+  try {
+    return jwtDecode(token);
+  } catch (error) {
+    console.error('Token inválido', error)
+    return null;
+  }
+}
+
+const isTokenExpired = (token: string) => {
+  const decoded = decodeToken(token);
+  if (!decoded || !decoded.exp) {
+    return true;
+  }
+  return Date.now() >= decoded.exp * 1000;
+}
+
+export const authService = {
+  login,
+  signup,
+  decodeToken,
+  isTokenExpired
 }
