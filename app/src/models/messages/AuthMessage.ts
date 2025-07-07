9c13fb018977529196eb90a5cf3920beaa218928
eduardocaas <eduardofc.dev@gmail.com>
2025-07-06 23:34:03 -0300

    Add AuthMessages

---
+++ app/src/models/messages/AuthMessage.ts
@@ -0,0 +1,9 @@
+export const AuthMessage = {
+  SIGNIN_ERROR_404: "Email ou senha inválidos",
+  SIGNIN_ERROR_400: "Email e senha obrigatórios",
+
+  SIGNUP_ERROR_400: "Campos obrigatórios: Nome, email e senha",
+  SIGNUP_ERROR_409: "Email já cadastrado no sistema",
+
+  ERROR_500: "Erro no servidor"
+}
