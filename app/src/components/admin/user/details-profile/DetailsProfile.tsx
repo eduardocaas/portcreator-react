f127b79f90796cba8778d9715de0cc477bb6d268
Eduardo <eduardofc.dev@gmail.com>
2025-07-08 00:29:32 -0300

    Implemented getUser

--- app/src/components/admin/user/details-profile/DetailsProfile.tsx
+++ app/src/components/admin/user/details-profile/DetailsProfile.tsx
@@ -2,7 +2,7 @@ import type React from "react";
 import type { User } from "../../../../models/admin/User";
 
 interface DetailsProfileProps {
-  user: User;
+  user: User | null;
 }
 
 const DetailsProfile: React.FC<DetailsProfileProps> = ({ user }) => {
@@ -11,40 +11,44 @@ const DetailsProfile: React.FC<DetailsProfileProps> = ({ user }) => {
     <>
       <h3>Informações</h3>
       <h5>Sobre</h5>
-      {user.description && (<p >{user.description}</p >)}
-      {!user.description && (<p className="text-secondary"> Adicione uma descrição</p >)}
-      <h5>Objetivo</h5>
-      {user.goal && (<p>{user.goal}</p >)}
-      {!user.goal && (<p className="text-secondary"> Adicione um objetivo</p>)}
-      <h5>Perfil</h5>
-      <table className="table table-striped-columns table-hover table-bordered">
-        <tbody>
-          <tr>
-            <td className="fw-bold col-1 col-sm-2">Nome</td>
-            <td>{user.name}</td>
-          </tr>
-          <tr>
-            <td className="fw-bold col-1 col-sm-2">Email</td>
-            <td>{user.email}</td>
-          </tr>
-          <tr>
-            <td className="fw-bold col-1 col-sm-2">Localização</td>
-            {user.location && (<td>{user.location}</td>)}
-            {!user.location && (<td className="text-secondary">Não informado</td>)}
-
-          </tr>
-          <tr>
-            <td className="fw-bold col-1 col-sm-2">Github</td>
-            {user.github && (<td>{user.github}</td>)}
-            {!user.github && (<td className="text-secondary">Não informado</td >)}
-          </tr >
-          <tr>
-            <td className="fw-bold col-1 col-sm-2">LinkedIn</td>
-            {user.linkedin && (<td>{user.linkedin}</td>)}
-            {!user.linkedin && (<td className="text-secondary">Não informado</td >)}
-          </tr >
-        </tbody >
-      </table >
+      {!user && <p className="text-secondary">Usuário não encontrado.</p>}
+      {user && (
+        <>
+          {user.description && (<p>{user.description}</p>)}
+          {!user.description && (<p className="text-secondary"> Adicione uma descrição</p>)}
+          <h5>Objetivo</h5>
+          {user.goal && (<p>{user.goal}</p>)}
+          {!user.goal && (<p className="text-secondary"> Adicione um objetivo</p>)}
+          <h5>Perfil</h5>
+          <table className="table table-striped-columns table-hover table-bordered">
+            <tbody>
+              <tr>
+                <td className="fw-bold col-1 col-sm-2">Nome</td>
+                <td>{user.name}</td>
+              </tr>
+              <tr>
+                <td className="fw-bold col-1 col-sm-2">Email</td>
+                <td>{user.email}</td>
+              </tr>
+              <tr>
+                <td className="fw-bold col-1 col-sm-2">Localização</td>
+                {user.location && (<td>{user.location}</td>)}
+                {!user.location && (<td className="text-secondary">Não informado</td>)}
+              </tr>
+              <tr>
+                <td className="fw-bold col-1 col-sm-2">Github</td>
+                {user.github && (<td>{user.github}</td>)}
+                {!user.github && (<td className="text-secondary">Não informado</td>)}
+              </tr>
+              <tr>
+                <td className="fw-bold col-1 col-sm-2">LinkedIn</td>
+                {user.linkedin && (<td>{user.linkedin}</td>)}
+                {!user.linkedin && (<td className="text-secondary">Não informado</td>)}
+              </tr>
+            </tbody>
+          </table>
+        </>
+      )}
     </>
   )
 }
--- app/src/components/admin/user/profile/Profile.tsx
+++ app/src/components/admin/user/profile/Profile.tsx
@@ -3,19 +3,18 @@ import { Button, Container } from "react-bootstrap";
 import DetailsProfile from "../details-profile/DetailsProfile";
 import FormProfile from "../form-profile/FormProfile";
 import type { User } from "../../../../models/admin/User";
+import { useEffect, useState } from "react";
+import { userService } from "../../../../services/admin/UserService";
 
 const Profile: React.FC = () => {
 
-  let user: User = {
-    id: '',
-    name: '',
-    email: '',
-    description: '',
-    goal: '',
-    github: '',
-    linkedin: '',
-    location: ''
-  }
+  const [user, setUser] = useState<User | null>(null)
+
+  useEffect(() => {
+    userService.getById().then(
+      u => setUser(u)
+    )
+  })
 
   return (
     <Container className="container mt-5">
@@ -23,7 +22,7 @@ const Profile: React.FC = () => {
         className="me-2"></i>Editar perfil</Button >
       <FormProfile />
       <hr />
-      <DetailsProfile user={user}/>
+      <DetailsProfile user={user} />
     </Container >
   );
 }
